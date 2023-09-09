1a71c894ab51a382b1ad43225ee87e6e2662c7d2
kollarm02 <114418339+kollarm02@users.noreply.github.com>
2023-09-09 19:45:47 +0200

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Game from './Game.svelte';
+  </script>
+  
+  <main>
+	<Game />
+  </main>
+  
+  <style>
+	main {
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	  height: 100vh;
+	  background-color: chartreuse;
+	}
+  </style>
+  
