68fbb7518b5501333c299a0d4195b6c482a6d510
kollarm02 <114418339+kollarm02@users.noreply.github.com>
2023-09-13 18:34:32 +0200

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<!-- src/App.svelte -->
+<script>
+	import Game from './Game.svelte';
+  </script>
+  
+  <main>
+	<Game />
+  </main>
+  
+  <style>
+	/* Stílusoldal */
+	main {
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	  height: 100vh;
+	  background-color:darkseagreen;
+	}
+  </style>
+  
